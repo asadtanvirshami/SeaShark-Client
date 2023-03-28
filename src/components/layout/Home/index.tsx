b9ec6c1df9f620149013e9375d5587c90ee47dd0
Asad Tanvir Shami <asadworkemail@gmail.com>
2023-03-28 20:56:18 +0500

    push

--- src/components/layout/Home/index.tsx
+++ src/components/layout/Home/index.tsx
@@ -1,16 +1,32 @@
-import React from 'react'
+import React from "react";
+import Image from 'next/image'
 
-type Props = {}
+import img from '../../../../public/images/revolt-164_6wVEHfI-unsplash (1).png'
+
+type Props = {};
 
 export const Home = (props: Props) => {
   return (
-    <div>
-      <div className='flex flex-col text-center align-middle justify-center mt-20'>
-        <h1 className=' text-4xl font-bold  text-blue-700 '>Hire the professionals for your work.</h1>
-        <p className='mt-4 text-2xl font-semibold'>Work must be done with perfection!</p>
+    <div className="container mx-auto">
+      <div className="flex flex-col text-center align-middle justify-center mt-20">
+        <h1 className=" text-4xl font-bold  text-blue-500 ">
+          Hire the professionals for your work.
+        </h1>
+        <p className="mt-4 text-2xl font-semibold">
+          Work must be done with perfection!
+        </p>
       </div>
+      <div className=" grid grid-cols-3 gap-4 text-center content-center">
+  <div className="..."><Image src={img} alt="#" className="rounded min-w-full	h-30	"/></div>
+  <div className="..."><Image src={img} alt="#" className=" rounded min-w-full h-30"/></div>
+  <div className="..."><Image src={img} alt="#" className=" rounded min-w-full h-30"/></div>
+  <div className="col-span-2 ..."><Image src={img} alt="#" className="rounded min-w-full h-11"/></div>
+  <div className="..."><Image src={img} alt="#" className="rounded min-w-full	 h-100"/></div>
+  <div className="..."><Image src={img} alt="#" className="rounded min-w-full	 h-30"/></div>
+  <div className="col-span-2 ..."><Image src={img} alt="#" className="rounded min-w-full h-30"/></div>
+</div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,160 +1,278 @@
-  import React, { useState } from "react";
-
-  type Props = {};
-
-  const Header = (props: Props) => {
-    const [show, setshow] = useState<Boolean>(false);
-    return (
-      <>
-        <div className="shadow-md">
-
-          <nav className="2xl: 2xl:mx-auto sm:py-6 sm:px-7 py-5 px-4 ">
-            {/* For large and Medium-sized Screen container */}
-            <div
-              id="bgIcon"
-              className={`focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 sm:hidden cursor-pointer grid grid-cols-2`}
+import React from "react";
+import {
+  Navbar,
+  MobileNav,
+  Typography,
+  Button,
+  Menu,
+  MenuHandler,
+  MenuList,
+  MenuItem,
+  Avatar,
+  Card,
+  IconButton,
+} from "@material-tailwind/react";
+import {
+  CubeTransparentIcon,
+  UserCircleIcon,
+  CodeBracketSquareIcon,
+  Square3Stack3DIcon,
+  ChevronDownIcon,
+  Cog6ToothIcon,
+  InboxArrowDownIcon,
+  LifebuoyIcon,
+  PowerIcon,
+  RocketLaunchIcon,
+  Bars2Icon,
+} from "@heroicons/react/24/outline";
+ 
+// profile menu component
+const profileMenuItems = [
+  {
+    label: "My Profile",
+    icon: UserCircleIcon,
+  },
+  {
+    label: "Edit Profile",
+    icon: Cog6ToothIcon,
+  },
+  {
+    label: "Inbox",
+    icon: InboxArrowDownIcon,
+  },
+  {
+    label: "Help",
+    icon: LifebuoyIcon,
+  },
+  {
+    label: "Sign Out",
+    icon: PowerIcon,
+  },
+];
+ 
+function ProfileMenu() {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+  const closeMenu = () => setIsMenuOpen(false);
+ 
+  return (
+    <Menu open={isMenuOpen} handler={setIsMenuOpen} placement="bottom-end">
+      <MenuHandler>
+        <Button
+          variant="text"
+          color="blue-gray"
+          className="flex items-center gap-1 rounded-full py-0.5 pr-2 pl-0.5 lg:ml-auto"
+        >
+          <Avatar
+            variant="circular"
+            size="sm"
+            alt="candice wu"
+            className="border border-blue-500 p-0.5"
+            src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
+          />
+          <ChevronDownIcon
+            strokeWidth={2.5}
+            className={`h-3 w-3 transition-transform ${
+              isMenuOpen ? "rotate-180" : ""
+            }`}
+          />
+        </Button>
+      </MenuHandler>
+      <MenuList className="p-1">
+        {profileMenuItems.map(({ label, icon }, key) => {
+          const isLastItem = key === profileMenuItems.length - 1;
+          return (
+            <MenuItem
+              key={label}
+              onClick={closeMenu}
+              className={`flex items-center gap-2 rounded ${
+                isLastItem
+                  ? "hover:bg-red-500/10 focus:bg-red-500/10 active:bg-red-500/10"
+                  : ""
+              }`}
             >
-              <div className="sm: text-left">
-                <h1>SharkBite</h1>
-              </div>
-              <div className="sm: flex justify-end">
-              <svg
-              onClick={() => setshow(!show)}
-                className={`${show ? "hidden" : ""}`}
-                width={24}
-                height={24}
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
+              {React.createElement(icon, {
+                className: `h-4 w-4 ${isLastItem ? "text-red-500" : ""}`,
+                strokeWidth: 2,
+              })}
+              <Typography
+                as="span"
+                variant="small"
+                className="font-normal"
+                color={isLastItem ? "red" : "inherit"}
               >
-                <path
-                  className=" transform duration-150"
-                  d="M4 6H20"
-                  stroke="#1F2937"
-                  strokeWidth="1.5"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-                <path
-                  d="M4 12H20"
-                  stroke="#1F2937"
-                  strokeWidth="1.5"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-                <path
-                  className=" transform duration-150"
-                  d="M4 18H20"
-                  stroke="#1F2937"
-                  strokeWidth="1.5"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-              <svg
-              onClick={() => setshow(!show)}
-                className={`${show ? "block" : "hidden"}`}
-                width={24}
-                height={24}
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M18 6L6 18"
-                  stroke="#1F2937"
-                  strokeWidth="1.5"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-                <path
-                  d="M6 6L18 18"
-                  stroke="#1F2937"
-                  strokeWidth="1.5"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-              </div>
-         
-           
-              
-            </div>
-            <div className="flex justify-between ">
-              <div className="hidden sm:flex flex-row items-center space-x-6">
-                <ul className="flex inline-block">
-                  <li
-                    className="relative before:content-[''] before:absolute before:block before:w-full before:h-[1.5px] 
-                before:bottom-0 before:left-0 before:bg-blue-500 
-                before:hover:scale-x-100 before:scale-x-0 before:origin-top-left
-                before:transition before:ease-in-out before:duration-300  mr-3 font-semibold cursor-pointer hover:text-blue-500 "
-                  >
-                    Home
-                  </li>
-                  <li
-                    className="relative before:content-[''] before:absolute before:block before:w-full before:h-[1.5px]
-                before:bottom-0 before:left-0 before:bg-blue-500 
-                before:hover:scale-x-100 before:scale-x-0 before:origin-top-left
-                before:transition before:ease-in-out before:duration-300  mr-3 font-semibold cursor-pointer hover:text-blue-500 "
-                  >
-                    Explore
-                  </li>
-                  <li
-                    className="relative before:content-[''] before:absolute before:block before:w-full before:h-[1.5px]
-                before:bottom-0 before:left-0 before:bg-blue-500 
-                before:hover:scale-x-100 before:scale-x-0 before:origin-top-left
-                before:transition before:ease-in-out before:duration-300  mr-3 font-semibold cursor-pointer hover:text-blue-500 "
-                  >
-                    Services
-                  </li>
-                  <li
-                    className="relative before:content-[''] before:absolute before:block before:w-full before:h-[1.5px]
-                before:bottom-0 before:left-0 before:bg-blue-500 
-                before:hover:scale-x-100 before:scale-x-0 before:origin-top-left
-                before:transition before:ease-in-out before:duration-300  mr-3 font-semibold cursor-pointer hover:text-blue-500 "
-                  >
-                    Pricing
-                  </li>
-                </ul>
-              </div>
-              <div className="hidden sm:flex flex-row items-center space-x-6">
-              <h1 className=" text-2xl  text-blue-700 font-bold">Shark Bite</h1>
-              </div>
-              <div className="hidden sm:flex flex-row items-center space-x-6">
-                <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-1 px-6 border border-blue-500 hover:border-transparent rounded transition-all duration-300 ease-in-out">
-                  Become a SharkBite!
-                </button>
-              </div>
-            </div>
-            {/* Mobile and small-screen devices (toggle Menu) */}
-            <div
-              id="MobileNavigation"
-              className={`${show ? "block" : "hidden"} sm:hidden mt-4 mx-auto`}
-            >
-        <ul className="">
-                <li className=" mr-3 mt-3 font-semibold cursor-pointer hover:text-blue-500 ">
-                  Home
-                </li>
-                <li className=" mr-3 mt-3 font-semibold cursor-pointer hover:text-blue-500 ">
-                  Explore
-                </li>
-                <li className=" mr-3 mt-3 font-semibold cursor-pointer hover:text-blue-500 ">
-                  Services
-                </li>
-                <li className=" mr-3 mt-3 font-semibold cursor-pointer hover:text-blue-500 ">
-                  Pricing
-                </li>
-              </ul>
-              <div className="sm:flex flex-row text-center space-x-6">
-              <button className="mt-5 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-1 px-6 border border-blue-500 hover:border-transparent rounded transition-all duration-300 ease-in-out">
-                  Become a SharkBite!
-                </button>
-              </div>
-            </div>
-          </nav>
-        </div>
-      </>
-    );
+                {label}
+              </Typography>
+            </MenuItem>
+          );
+        })}
+      </MenuList>
+    </Menu>
+  );
+}
+ 
+// nav list menu
+const navListMenuItems = [
+  {
+    title: "@material-tailwind/html",
+    description:
+      "Learn how to use @material-tailwind/html, packed with rich components and widgets.",
+  },
+  {
+    title: "@material-tailwind/react",
+    description:
+      "Learn how to use @material-tailwind/react, packed with rich components for React.",
+  },
+  {
+    title: "Material Tailwind PRO",
+    description:
+      "A complete set of UI Elements for building faster websites in less time.",
+  },
+];
+ 
+function NavListMenu() {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+ 
+  const triggers = {
+    onMouseEnter: () => setIsMenuOpen(true),
+    onMouseLeave: () => setIsMenuOpen(false),
   };
-
-  export default Header;
+ 
+  const renderItems = navListMenuItems.map(({ title, description }) => (
+    <a href="#" key={title}>
+      <MenuItem>
+        <Typography variant="h6" color="blue-gray" className="mb-1">
+          {title}
+        </Typography>
+        <Typography variant="small" color="gray" className="font-normal">
+          {description}
+        </Typography>
+      </MenuItem>
+    </a>
+  ));
+ 
+  return (
+    <React.Fragment>
+      <Menu open={isMenuOpen} handler={setIsMenuOpen}>
+        <MenuHandler>
+          <Typography as="a" href="#" variant="small" className="font-normal">
+            <MenuItem
+              {...triggers}
+              className="hidden items-center gap-2 text-blue-gray-900 lg:flex lg:rounded-full"
+            >
+              <Square3Stack3DIcon className="h-[18px] w-[18px]" /> Categories{" "}
+              <ChevronDownIcon
+                strokeWidth={2}
+                className={`h-3 w-3 transition-transform ${
+                  isMenuOpen ? "rotate-180" : ""
+                }`}
+              />
+            </MenuItem>
+          </Typography>
+        </MenuHandler>
+        <MenuList
+          {...triggers}
+          className="hidden w-[36rem] grid-cols-7 gap-3 overflow-visible lg:grid"
+        >
+          <Card
+            color="blue"
+            shadow={false}
+            variant="gradient"
+            className="col-span-3 grid h-full w-full place-items-center rounded-md"
+          >
+            <RocketLaunchIcon strokeWidth={1} className="h-28 w-28" />
+          </Card>
+          <ul className="col-span-4 flex w-full flex-col gap-1">
+            {renderItems}
+          </ul>
+        </MenuList>
+      </Menu>
+      <MenuItem className="flex items-center gap-2 text-blue-gray-900 lg:hidden">
+        <Square3Stack3DIcon className="h-[18px] w-[18px]" /> Categories{" "}
+      </MenuItem>
+      <ul className="ml-6 flex w-full flex-col gap-1 lg:hidden">
+        {renderItems}
+      </ul>
+    </React.Fragment>
+  );
+}
+ 
+// nav list component
+const navListItems = [
+  {
+    label: "Account",
+    icon: UserCircleIcon,
+  },
+  {
+    label: "Services",
+    icon: CubeTransparentIcon,
+  },
+  {
+    label: "Pricing",
+    icon: CodeBracketSquareIcon,
+  },
+];
+ 
+function NavList() {
+  return (
+    <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center">
+      <NavListMenu />
+      {navListItems.map(({ label, icon }, key) => (
+        <Typography
+          key={label}
+          as="a"
+          href="#"
+          variant="small"
+          color="blue-gray"
+          className="font-normal"
+        >
+          <MenuItem className="flex items-center gap-2 lg:rounded-full">
+            {React.createElement(icon, { className: "h-[18px] w-[18px]" })}{" "}
+            {label}
+          </MenuItem>
+        </Typography>
+      ))}
+    </ul>
+  );
+}
+ 
+export default function ComplexNavbar() {
+  const [isNavOpen, setIsNavOpen] = React.useState(false);
+  const toggleIsNavOpen = () => setIsNavOpen((cur) => !cur);
+ 
+  React.useEffect(() => {
+    window.addEventListener(
+      "resize",
+      () => window.innerWidth >= 960 && setIsNavOpen(false)
+    );
+  }, []);
+ 
+  return (
+    <Navbar className="mx-auto max-w-screen-xl p-2 lg:rounded-full lg:pl-6 mt-3">
+      <div className="relative mx-auto flex items-center text-blue-gray-900">
+        <Typography
+          as="a"
+          href="#"
+          className="mr-4 ml-2 cursor-pointer py-1.5 font-medium text-blue-500 font-semibold"
+        >
+          Shark Bite
+        </Typography>
+        <div className="absolute top-2/4 left-2/4 hidden -translate-x-2/4 -translate-y-2/4 lg:block">
+          <NavList />
+        </div>
+        <IconButton
+          size="sm"
+          color="blue-gray"
+          variant="text"
+          onClick={toggleIsNavOpen}
+          className="ml-auto mr-2 lg:hidden"
+        >
+          <Bars2Icon className="h-6 w-6" />
+        </IconButton>
+        <ProfileMenu />
+      </div>
+      <MobileNav open={isNavOpen} className="overflow-scroll">
+        <NavList />
+      </MobileNav>
+    </Navbar>
+  );
+}
